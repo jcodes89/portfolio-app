302bc2e906f90462332ad3207ce682f8574652cd
jcodes89 <jeremy.sluder89@gmail.com>
2024-04-13 12:28:59 -0400

    update to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
    
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('/Images/pawel-czerwinski-fRzUPSFnp04-unsplash.jpg')",
+        "hero-pattern": "url('/images/pawel-czerwinski-fRzUPSFnp04-unsplash.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
