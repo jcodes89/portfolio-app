761f3b993617bbe89f7ab5e88b307ffb9d365775
jcodes89 <jeremy.sluder89@gmail.com>
2024-04-13 13:05:35 -0400

    update to about page

--- app/about/page.js
+++ app/about/page.js
@@ -104,7 +104,7 @@ const page = () => {
         <p className='lg:hidden text-xl flex  items-center '><MdSwipeRight/><span className='text-sm'>Swipe Right</span></p>
         <h6 className='font-bold text-xl py-2'>Technologies</h6>
       </div>
-      <div className='lg:hidden techContainer flex flex-row   py-4 text-6xl text-accent w-full h-fit'>
+      <div className='lg:hidden techContainer flex flex-row overflow-hidden   py-4 text-6xl text-accent w-full h-fit'>
         <div className='techItems flex flex-row justify-start  cursor-pointer'>
           <div className='px-2 justify-center items-center flex flex-col hover:text-gray-50'><SiHtml5/><span className='text-base'>Html</span></div>
           <div className='px-2 justify-center items-center flex flex-col hover:text-gray-50'><SiCss3/><span className='text-base'>Css</span></div>
